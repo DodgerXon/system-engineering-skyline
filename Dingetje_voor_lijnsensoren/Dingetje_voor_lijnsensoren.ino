d71dc41dbced7a27c1ee03cbd99f9273abbb0a69
Dansther <d.a.n.i.e.l@casema.nl>
2023-06-05 14:14:48 +0200

    a

--- Dingetje_voor_lijnsensoren/Dingetje_voor_lijnsensoren.ino
+++ Dingetje_voor_lijnsensoren/Dingetje_voor_lijnsensoren.ino
@@ -1,6 +1,9 @@
 #include <Wire.h>
+#include "QTRSensors.cpp"
+#include "QTRSensors.h"
 #include <Zumo32U4.h>
-#include "Calibrateding.cpp"
+//#include "Calibrateding.cpp"
+
 
 const uint16_t maxSpeed = 400;
 
@@ -14,7 +17,7 @@ Zumo32U4ButtonB buttonB;
 
 int calibrationSensors[2];
 
-unsigned int lineSensorValues[5];
+unsigned int sensorWaardes[5];
 
 int zwart[2];
 
@@ -48,42 +51,42 @@ void calibrate()
 {
   buttonA.waitForButton();
   for (int i = 0; i < 2; i++) {
-  lineSensors.calibrate(&zwart[0], &zwart[1]);
+  lineSensors.Calibrate(zwart[0], zwart[1]);
   delay(2000);
   buzzer.play(">g32>>c32");
   }
 
 buttonA.waitForButton();
   for (int i = 0; i < 2; i++) {
-  lineSensors.calibrate(&bruin[0], &bruin[1]);
+  lineSensors.Calibrate(bruin[0], bruin[1]);
   delay(2000);
   buzzer.play(">g32>>c32");
   }
 
 buttonA.waitForButton();
 for (int i = 0; i < 2; i++) {
-  lineSensors.calibrate(&grijs[0], &grijs[1]);
+  lineSensors.Calibrate(grijs[0], grijs[1]);
   delay(2000);
   buzzer.play(">g32>>c32");
 }
 
 buttonA.waitForButton();
 for (int i = 0; i < 2; i++) {
-  lineSensors.calibrate(&rood[0], &rood[1]);
+  lineSensors.Calibrate(rood[0], rood[1]);
   delay(2000);
   buzzer.play(">g32>>c32");
 }
 
 buttonA.waitForButton();
 for (int i = 0; i < 2; i++) {
-  lineSensors.calibrate(&groen[0], &groen[1]);
+  lineSensors.Calibrate(groen[0], groen[1]);
   delay(2000);
   buzzer.play(">g32>>c32");
 }
 
 buttonA.waitForButton();
 for (int i = 0; i < 2; i++) {
-  lineSensors.calibrate(&wit[0], &wit[1]);
+  lineSensors.Calibrate(wit[0], wit[1]);
   delay(2000);
   buzzer.play(">g32>>c32");
 }
@@ -91,13 +94,13 @@ buttonB.waitForButton();
 
 while(!buttonA.getSingleDebouncedPress())
   {
-    lineSensors.readCalibrated(lineSensorValues);
+    lineSensors.readCalibrated(sensorWaardes);
   }
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-    int16_t position = lineSensors.readLine(lineSensorValues);
+    int16_t position = lineSensors.readLine(sensorWaardes);
 
     int16_t error = position - 2000;
 
@@ -105,7 +108,7 @@ void loop() {
 
   lastError = error;
 
-    if (sensorWaardes[2] >= zwart[0] && sensorWaardes[2] <= zwart[1] && sensorWaardes[3] >= wit[0] && sensorWaardes[3] <= wit[1] && sensorWaardes[4] >= wit[0] && sensorWaardes[4] <= wit[1] && sensorWaardes[1] >= wit[0] && sensorWaardes[1] <= wit[1] && sensorWaardes[0] >= wit[0] && sensorWaardes[0] <= wit[1]  && grijsOfBruin == 0) {
+    if (sensorWaardes[2] >= zwart[0] && sensorWaardes[2] <= zwart[1] && sensorWaardes[3] >= wit[0] && sensorWaardes[3] <= wit[1] && sensorWaardes[4] >= wit[0] && sensorWaardes[4] <= wit[1] && sensorWaardes[1] >= wit[0] && sensorWaardes[1] <= wit[1] && sensorWaardes[0] >= wit[0] && sensorWaardes[0] <= wit[1]) {
   
   //zwarte lijn in het midden volgen
 
@@ -118,33 +121,33 @@ void loop() {
     motors.setSpeeds(leftSpeed, rightSpeed);
 }
 
-if (sensorWaardes[2] >= wit[0] && sensorWaardes[2] <= wit[1] && sensorWaardes[3] >= wit[0] && sensorWaardes[3] <= wit[1] && sensorWaardes[4] >= wit[0] && sensorWaardes[4] <= wit[1] && sensorWaardes[1] >= zwart[0] && sensorWaardes[1] <= zwart[1] && sensorWaardes[0] >= wit[0] && sensorWaardes[0] <= wit[1]  && grijsOfBruin == 0) {
+if (sensorWaardes[2] >= wit[0] && sensorWaardes[2] <= wit[1] && sensorWaardes[3] >= wit[0] && sensorWaardes[3] <= wit[1] && sensorWaardes[4] >= wit[0] && sensorWaardes[4] <= wit[1] && sensorWaardes[1] >= zwart[0] && sensorWaardes[1] <= zwart[1] && sensorWaardes[0] >= wit[0] && sensorWaardes[0] <= wit[1]) {
   
   //sturen naar links
 }
 
-if (sensorWaardes[2] >= zwart[0] && sensorWaardes[2] <= zwart[1] && sensorWaardes[3] >= grijs[0] && sensorWaardes[3] <= grijs[1] && sensorWaardes[4] >= grijs[0] && sensorWaardes[4] <= grijs[1] && grijsOfBruin == 0) {
+if (sensorWaardes[2] >= zwart[0] && sensorWaardes[2] <= zwart[1] && sensorWaardes[3] >= grijs[0] && sensorWaardes[3] <= grijs[1] && sensorWaardes[4] >= grijs[0] && sensorWaardes[4] <= grijs[1]) {
   
   // volgende kruispunt rechts
 } 
 
-if (sensorWaardes[2] >= zwart[0] && sensorWaardes[2] <= zwart[1] && sensorWaardes[1] >= grijs[0] && sensorWaardes[1] <= grijs[1] && sensorWaardes[0] >= grijs[0] && sensorWaardes[0] <= grijs[1] && grijsOfBruin == 0) {
+if (sensorWaardes[2] >= zwart[0] && sensorWaardes[2] <= zwart[1] && sensorWaardes[1] >= grijs[0] && sensorWaardes[1] <= grijs[1] && sensorWaardes[0] >= grijs[0] && sensorWaardes[0] <= grijs[1]) {
   
   // volgende kruispunt links
 } 
 
-if (sensorWaardes[2] >= zwart[0] && sensorWaardes[2] <= zwart[1] && sensorWaardes[3] >= grijs[0] && sensorWaardes[3] <= grijs[1] && sensorWaardes[4] >= grijs[0] && sensorWaardes[4] <= grijs[1] && sensorWaardes[1] >= grijs[0] && sensorWaardes[1] <= grijs[1] && sensorWaardes[0] >= grijs[0] && sensorWaardes[0] <= grijs[1]  && grijsOfBruin == 0) {
+if (sensorWaardes[2] >= zwart[0] && sensorWaardes[2] <= zwart[1] && sensorWaardes[3] >= grijs[0] && sensorWaardes[3] <= grijs[1] && sensorWaardes[4] >= grijs[0] && sensorWaardes[4] <= grijs[1] && sensorWaardes[1] >= grijs[0] && sensorWaardes[1] <= grijs[1] && sensorWaardes[0] >= grijs[0] && sensorWaardes[0] <= grijs[1]) {
   
   //Stilstaan voor WipWap en wachten op IMU = 0 (of whatever de waarde ervoor is)
 } 
 
-if (sensorWaardes[2] >= zwart[0] && sensorWaardes[2] <= zwart[1] && sensorWaardes[3] >= bruin[0] && sensorWaardes[3] <= bruin[1] && sensorWaardes[4] >= bruin[0] && sensorWaardes[4] <= bruin[1] && sensorWaardes[1] >= bruin[0] && sensorWaardes[1] <= bruin[1] && sensorWaardes[0] >= bruin[0] && sensorWaardes[0] <= bruin[1]  && grijsOfBruin == 1) {
+if (sensorWaardes[2] >= zwart[0] && sensorWaardes[2] <= zwart[1] && sensorWaardes[3] >= bruin[0] && sensorWaardes[3] <= bruin[1] && sensorWaardes[4] >= bruin[0] && sensorWaardes[4] <= bruin[1] && sensorWaardes[1] >= bruin[0] && sensorWaardes[1] <= bruin[1] && sensorWaardes[0] >= bruin[0] && sensorWaardes[0] <= bruin[1]) {
 
   //20 cm vooruitrijden
   //fase 2
 }
 
-if (sensorWaardes[2] >= bruin[0] && sensorWaardes[2] <= bruin[1] && grijsOfBruin == 1) {
+if (sensorWaardes[2] >= bruin[0] && sensorWaardes[2] <= bruin[1]) {
   
 }
 if (sensorWaardes[2] >= rood[0] && sensorWaardes[2] <= rood[1]) {
@@ -153,9 +156,8 @@ if (sensorWaardes[2] >= rood[0] && sensorWaardes[2] <= rood[1]) {
 
 if (sensorWaardes[2] >= groen[0] && sensorWaardes[2] <= groen[1]) {
   //snelheid = 50%
-  grijsOfBruin = 1;
 } 
-if (sensorWaardes[2] >= grijs[0] && sensorWaardes[2] <= grijs[1] && grijsOfBruin == 0) {
+if (sensorWaardes[2] >= grijs[0] && sensorWaardes[2] <= grijs[1]) {
   
 }
 }
