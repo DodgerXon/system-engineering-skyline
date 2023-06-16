9f360db9fb4b44519a04e2976c8a66f3d3c2674e
Dansther <d.a.n.i.e.l@casema.nl>
2023-06-16 17:50:16 +0200

    Code voorzien van commentaar

--- FInal version/Line/Line.ino
+++ FInal version/Line/Line.ino
@@ -2,7 +2,7 @@
 #include "Proximity.h"
 #include "IMU.h"
 
-Line linesensor;
+Line linesensor; //construeren objecten
 Line motors;
 
 Proximity prox;
@@ -11,26 +11,25 @@ IMU imusensor;
 
 int speedL = 200;
 int speedR = -200;
-char input = Serial1.read();
+char input = Serial1.read(); //voor xbee, dat hij dingen in kan lezen
 
-int waardes[5];
+int waardes[5]; //Dit geldt voor alles hieronder, Arraylengte, en om de waardes in de main te krijgen en te gebruiken in methodes.
 int zwart[2];
 int groen[2];
 int bruin[2];
 int wit[2];
 int grijs[2];
-int switchm = 0;
+int switchm = 0; //voor switch
 
 void setup() {
-  linesensor.setup(groen[0], groen[1], zwart[0], zwart[1], bruin[0], bruin[1], wit[0], wit[1], grijs[0], grijs[1]); // calibreert de verschillende kleuren, en returned de waarden die hiern
-  imusensor.klaarmaken();
+  linesensor.setup(groen[0], groen[1], zwart[0], zwart[1], bruin[0], bruin[1], wit[0], wit[1], grijs[0], grijs[1]); // calibreert de verschillende kleuren, en returned de waarden die hierin, die boven worden opgeslagen.
 }
 
 void loop() {
-  input = Serial1.read();
-  linesensor.lezen(waardes[0], waardes[1], waardes[2], waardes[3], waardes[4]);
+  input = Serial1.read(); //voor fase 2, zo blijft hij het inlezen
+  linesensor.lezen(waardes[0], waardes[1], waardes[2], waardes[3], waardes[4]); //Deze methode zorgt ervoor dat de sensorwaardes in de main komt.
  
-  for (int i = 0; i < 5; i++) {
+  for (int i = 0; i < 5; i++) { //for loop om te kijken welke switch case actief moet zijn, het eronder vergelijkt waardes, om zo de case te bepalen.
     if (waardes[i] <= zwart[1] && waardes[i] >= wit[0])  {
       switchm = 3;
     }
@@ -52,25 +51,25 @@ void loop() {
     }
   }
 
-  switch(switchm) {
-    case 1:
+  switch(switchm) { //switch voor verschillende cases, om de andere uit te sluiten en maar 1 ding uit te voeren.
+    case 1: //Groenelijn
       linesensor.LineRijdenGroen();
       break;
-    case 2:
+    case 2: //bruinelijn
       linesensor.LineRijdenBruin();
       break;
-    case 3:
+    case 3: //zwartelijn
       linesensor.LineRijdenzwart();
       break;
-    case 4:
+    case 4: //fase 2 (proximity gebruiken)
       prox.printReadingsToSerial();
       prox.uitvoeren();
     break;
-    case 5:
+    case 5://imu gebruiken
       imusensor.lezen();
       imusensor.printen();
       imusensor.balans();
     break;
   }
-  Serial1.println(switchm);
+  Serial1.println(switchm); //printen van de switch voor debugging purposes
 }
